65ffb0f9925607a736e47291abdb4c086cc10944
Milt Deherrera <miltdeherrera@gmail.com>
2022-11-10 15:12:51 -0800

    add text area and phone type fields to form

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -5,6 +5,8 @@ const ContactUs = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
+    const [phoneType, setPhoneType] = useState('');
+    const [comments, setComments] = useState('');
 
     const onSubmit = (e) => {
         e.preventDefault();
@@ -13,6 +15,8 @@ const ContactUs = () => {
             name,
             email,
             phone,
+            phoneType,
+            comments,
             submittedOn: new Date()
         };
 
@@ -21,6 +25,8 @@ const ContactUs = () => {
         setName('');
         setEmail('');
         setPhone('');
+        setPhoneType('');
+        setComments('');
     }
 
     return (
@@ -51,6 +57,27 @@ const ContactUs = () => {
                         type='text'
                         onChange={e => setPhone(e.target.value)}
                         value={phone} />
+                    <select
+                        name='phoneType'
+                        onChange={e => setPhoneType(e.target.value)}
+                        value={phoneType}
+                    >
+                        <option value='' disabled>
+                            Select a phone type...
+                        </option>
+                        <option>Home</option>
+                        <option>Work</option>
+                        <option>Mobile</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor='comments'>Comments:</label>
+                    <textarea
+                        id='comments'
+                        name='comments'
+                        onChange={e => setComments(e.target.value)}
+                        value={comments}
+                    />
                 </div>
                 <button>Submit</button>
             </form>
